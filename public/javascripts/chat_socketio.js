a2780c9ecb917c44add2a1fd13fe49f8afc20596
seregaNV <serega.n.v333@gmail.com>
2015-12-12 15:13:50 +0200

    Добавив реконект чята при втраті з'єднання.

--- public/javascripts/chat_socketio.js
+++ public/javascripts/chat_socketio.js
@@ -3,16 +3,29 @@ $(document).ready(function(){
     var socket = io.connect('', {'reconnection delay': 1});
     var form = $('#js_chat_publish');
     var ul = $('#js_chat_messages');
-    form.submit(function() {
-        var input = $(this).find(':input');
+    var input = $('#js_chat_input');
+    function sendMessage() {
         var text = input.val();
         input.val('');
-        socket.emit('message', text, function(data) {
+        socket.emit('message', text, function() {
             $('<li>', {text: text}).appendTo(ul);
         });
         return false;
-    });
-    socket.on('message', function(text) {
-        $('<li>', {text: text}).appendTo(ul);
-    });
+    }
+    socket
+        .on('message', function(message) {
+            $('<li>', {text: message}).appendTo(ul);
+        })
+        .on('connect', function() {
+            $('<li>', {text: 'connection is established...'}).appendTo(ul);
+            form.on('submit', sendMessage);
+            form.find('button').prop('disabled', false);
+            input.prop('disabled', false);
+        })
+        .on('disconnect', function() {
+            $('<li>', {text: 'connection is lost...'}).appendTo(ul);
+            form.off('submit', sendMessage);
+            form.find('button').prop('disabled', true);
+            input.prop('disabled', true);
+        });
 });
