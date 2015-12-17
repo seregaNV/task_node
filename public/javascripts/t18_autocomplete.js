3feb903b8da27029e5201e27e579d28260286e59
seregaNV <serega.n.v333@gmail.com>
2015-12-17 04:40:42 +0200

    Доліпив плагін автокомпліта.

--- public/javascripts/t18_autocomplete.js
+++ public/javascripts/t18_autocomplete.js
@@ -1,9 +1,45 @@
 "use strict";
 $(document).ready(function() {
 
-    $("input").myAutocomplete({
-        //pathToFile: '',
-        //inputType: 'text'
+    $("input.task_input").myAutocomplete({
+        pathToFile: '/task.json',
+        placeHolder: 'Company name',
+        colorStyle: 'selfStyle' //'warningStyle', 'errorStyle', 'defaultStyle', 'successStyle';
+        //inputName: 'autocomplete'
+        //inputType: 'email'
+        //autoFocus: true
+    });
+    $("input.default_style").myAutocomplete({
+        pathToFile: '/task.json',
+        placeHolder: 'Email address',
+        colorStyle: 'defaultStyle', //'warningStyle', 'errorStyle', 'selfStyle', 'successStyle';
+        inputType: 'email'
+        //inputName: 'autocomplete'
+        //autoFocus: true
+    });
+    $(".has-success input").myAutocomplete({
+        pathToFile: '/task.json',
+        placeHolder: 'Name',
+        colorStyle: 'successStyle' //'warningStyle', 'errorStyle', 'selfStyle', 'defaultStyle';
+        //inputType: 'email'
+        //inputName: 'autocomplete'
+        //autoFocus: true
+    });
+    $(".has-error input").myAutocomplete({
+        pathToFile: '/task.json',
+        placeHolder: 'Error',
+        colorStyle: 'errorStyle', //'successStyle', 'warningStyle', 'selfStyle', 'defaultStyle';
+        inputType: 'email'
+        //inputName: 'autocomplete'
+        //autoFocus: true
+    });
+    $(".has-warning").find('input').myAutocomplete({
+        pathToFile: '/task.json',
+        placeHolder: 'Name',
+        colorStyle: 'warningStyle' //'successStyle', 'errorStyle', 'selfStyle', 'defaultStyle';
+        //inputType: 'email'
+        //autoFocus: true
+        //inputName: 'autocomplete'
     });
 });
 
--- public/javascripts/t18_jquery_myAutocomplete.js
+++ public/javascripts/t18_jquery_myAutocomplete.js
@@ -3,61 +3,70 @@
     $.fn.myAutocomplete = function(options){
 
         var settings = {
-            pathToFile: '/task.json',
-            inputType: 'text'
+            pathToFile: '',
+            inputType: 'text',
+            inputName: 'query',
+            placeHolder: '',
+            autoFocus: false,
+            colorStyle: 'defaultStyle' //selfStyle, successStyle, warningStyle, errorStyle
         };
 
         return this.each(function() {
             if (options)$.extend(settings, options);
 
             var $this = $(this),
-                //$("input[type='text']")
                 $searchBox = $this,
                 $searchResult,
 
-                inputValueLength = 0,
+                inputValueLength,
+                inputValue = '',
                 suggestSelected = 0,
-                quantityOfResults = 0,
-                inputValue = '';
-            if ($this.attr('type') == settings.inputType) {
-                addElements()
-            }
+                quantityOfResults = 0;
+            if ($this.attr('type') == settings.inputType) addElements();
 
             function addElements() {
 
-                //<input type="text" class="form-control" id="exampleInputAmount" placeholder="Amount">
-                <!--<input type="text" name="query" class="form-control task_input" id="js_t18_search_box" autofocus placeholder="Company name" value="" autocomplete="off">-->
-                $searchBox.parent().addClass('myAutocomplete_search_input');//поміняти все на клас
+                $searchBox.parent().addClass('myAutocomplete_search_input');
 
 
-                $searchBox.addClass('js_myAutocomplete_search_box');//поміняти все на клас
-                //$searchBox.addClass('task_input');//подивитися чи потрібно?
-                $searchBox.attr('name', 'query');//поміняти на autocomplete
+                $searchBox.addClass('js_myAutocomplete_search_box');
                 $searchBox.attr('autocomplete', 'off');
-                $searchBox.attr('placeholder', 'Company name');//добавити в свойства
+                //$searchBox.addClass('task_input');//подивитися чи потрібно?
+                $searchBox.attr('name', settings.inputName);//поміняти на autocomplete
+                $searchBox.attr('placeholder', settings.placeHolder);
                 $searchBox.attr('value', '');
-                $searchBox.focus();//добавити в свойства
 
                 $searchBox.after('<div class="js_myAutocomplete_search_result"></div>');
                 $searchResult = $searchBox.next();
-                $searchResult.width($searchBox.outerWidth());
-
-                //$('.myAutocomplete_search_input').next('button').css({'position': 'static'});
-                //$this.append('<form action="" method="GET" class="form-inline" id="js_t18_form"></form>');
-                //$form = $this.children('#js_t18_form');
-                //$form.append('<div class="form-group"></div>');
-                //$form.children().append('<input type="text" name="query" class="form-control task_input" id="js_t18_search_box" autofocus placeholder="Company name" value="" autocomplete="off">');
-                //$searchBox = $form.find('#js_t18_search_box');
-                //$form.append('<button type="submit" class="btn btn-default">Search</button>');
-                //$form.append('<div class="js_myAutocomplete_search_result"></div>');
-                //$searchResult = $form.children(".js_myAutocomplete_search_result");
+                $searchResult.outerWidth($searchBox.outerWidth());
 
 
+                switch(settings.colorStyle) {
+                    case 'defaultStyle':
+                        $searchResult.addClass('js_myAutocomplete_default_style');
+                        break;
+                    case 'successStyle':
+                        $searchResult.addClass('js_myAutocomplete_success_style');
+                        break;
+                    case 'warningStyle':
+                        $searchResult.addClass('js_myAutocomplete_warning_style');
+                        break;
+                    case 'errorStyle':
+                        $searchResult.addClass('js_myAutocomplete_error_style');
+                        break;
+                    case 'selfStyle':
+                        $searchResult.addClass('js_myAutocomplete_self_style');
+                        break;
+                }
+
+
+                if (settings.autoFocus) $searchBox.focus();
                 keyUp();
                 listeners();
             }
 
             function keyUp() {
+                $searchResult.css({'left': $this.position().left});
                 $searchBox.keyup(function(I) {
                     if (((I.keyCode >= 48) && (I.keyCode <= 111)) || I.keyCode == 8) {
                         inputValue = $searchBox.val();
@@ -78,13 +87,14 @@
                                 quantityOfResults = $searchResult.children().length;
                             });
                         } else {
-                            //$searchResult.hide();
+                            $searchResult.hide();
                         }
                     }
                 });
             }
 
             function listeners() {
+
                 $searchBox.keydown(function(I){
                     switch(I.keyCode) {
                         case 27: // escape
@@ -95,7 +105,7 @@
                         case 40: // стрелка вниз
                             I.preventDefault();
                             if(quantityOfResults){
-                                key_activate( I.keyCode-39 );
+                                selecting( I.keyCode-39 );
                             }
                             break;
                     }
@@ -116,27 +126,25 @@
                 $searchResult.mouseover(function() {
                     $searchResult.find('div').removeClass('active');
                 });
-                function key_activate(n){
-                    var $searchResultDiv = $this.next('.js_myAutocomplete_search_result').find('div');
-                    $searchResultDiv.eq(suggestSelected-1).removeClass('active');
+            }
 
-                    if(n == 1 && suggestSelected < quantityOfResults){
-                        suggestSelected++;
-                    }else if(n == -1 && suggestSelected > 1){
-                        suggestSelected--;
-                    }
+            function selecting(n){
+                var $searchResultDiv = $this.next('.js_myAutocomplete_search_result').find('div');
+                $searchResultDiv.eq(suggestSelected-1).removeClass('active');
 
-                    if( suggestSelected > 0){
-                        $searchResultDiv.eq(suggestSelected-1).addClass('active');
-                        $searchBox.val( $searchResultDiv.eq(suggestSelected-1).text() );
-                    } else {
-                        $searchBox.val( inputValue );
-                    }
+                if(n == 1 && suggestSelected < quantityOfResults){
+                    suggestSelected++;
+                }else if(n == -1 && suggestSelected > 1){
+                    suggestSelected--;
                 }
-            };
 
-
-
+                if( suggestSelected > 0){
+                    $searchResultDiv.eq(suggestSelected-1).addClass('active');
+                    $searchBox.val( $searchResultDiv.eq(suggestSelected-1).text() );
+                } else {
+                    $searchBox.val( inputValue );
+                }
+            }
         });
     };
 })(jQuery);
